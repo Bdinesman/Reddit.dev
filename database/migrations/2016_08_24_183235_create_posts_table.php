3de0bfbbe5268c407a84dd01d548b7aabe823384
Brandon Dinesman <Bdinesman@Brandon-Dinesmans-MacBook-Pro.local>
2016-08-24 13:56:05 -0500

    Created migrations

---
+++ database/migrations/2016_08_24_183235_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use App\Post;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title',25);
+            $table->string('url',200)->default('');
+            $table->longtext('content');
+            $table->integer('created_by')->unsigned();
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
